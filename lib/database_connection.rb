67d16daa0a963ca4c5a6d860e172b8aca09efb56
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-07-14 17:06:19 +0100

    Web application setup

---
+++ lib/database_connection.rb
@@ -0,0 +1,15 @@
+def self.connect
+  # If the environment variable (set by Heroku)
+  # is present, use this to open the connection.
+  if ENV['DATABASE_URL'] != nil
+    @connection = PG.connect(ENV['DATABASE_URL'])
+    return
+  end
+
+  if ENV['ENV'] == 'test'
+    database_name = 'music_library_test'
+  else
+    database_name = 'music_library'
+  end
+  @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
+end
